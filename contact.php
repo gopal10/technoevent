42badfdcd5461b4582de168cbfe6ecdfcaa32a35
gopal10 <gopalpanchal10@gmail.com>
2017-04-29 19:47:14 +0530

    webpages

---
+++ contact.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <title>Welcome to TechnoEvent</title>
+
+       <link rel="stylesheet" href="css/style.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+ <link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="images/logo3.png">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <link rel="stylesheet" href="css/animated.css">
+  </head>
+  <body>
+   
+     <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+                <a class="navbar-brand" href="index.php">
+                    <div-col-xs-3><img src="images/logo3.png" alt="logo" width="35px"></div-col-xs-3>
+                    <div-col-xs-9>TechnoEvent</div-col-xs-9>
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="page1.php"><i class="fa fa-home" aria-hidden="true">Home</i>
+</a></li>
+                   
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+    <div class="jumbotron">
+        <div class="container">
+            <div id="detail" class="animated fadeInLeft">
+                <h1>TechnoEvent</h1>
+                <p> Welcome To the TechnoEvent.</p>
+            </div>
+        </div> <img src="images/re12.png" alt="jumbotron" width="100%"> </div>
+
+    
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                
+        
+                    <div class="row">
+                        
+                        
+                        <div class="col-md-12 contact-form">
+                           <h2>Contact Form <hr></h2>
+                            <form action="">
+                                <div class="form-group">
+                                    <label for="full-name">Full Form</label>
+                                    <input type="text" id="full-name" class="form-control" placeholder="Full Name">
+                                </div>
+                                
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="text" id="email" class="form-control" placeholder="Email Address">
+                                </div>
+                                
+                                <div class="form-group">
+                                    <label for="website">Web-Site</label>
+                                    <input type="text" id="website" class="form-control" placeholder="Website">
+                                </div>
+                                
+                                <div class="form-group">
+                                    <label for="message">Message:</label>
+                                    <textarea type="text" id="text" cols="30" rows="10" class="form-control" placeholder="Your Message is here">
+                                    </textarea>
+                                </div>
+                                <input type="submit" value="Submit" id="submit" class="btn btn-success">
+                                
+                            </form>
+                            
+                            
+                        </div>
+                    </div>
+                    
+                    
+                     
+                    </div>
+                
+                
+            </div>
+        </div>
+    </section>
+    <footer >
+    <div class="TechnoEvent"> <center>&copy; Copyrights By www.technoevent.com All Rights Reserved   </center></div>
+    </footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html
---
+++ page1.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <title>Welcome to TechnoEvent</title>
+
+       <link rel="stylesheet" href="css/style.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+ <link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="images/logo3.png">
+    <link rel="stylesheet" href="css/font-awesome.css">
+   
+    <link rel="stylesheet" href="css/animated.css">
+  </head>
+  <body>
+   
+     <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+                <a class="navbar-brand" href="index.php">
+                    <div-col-xs-3><img src="images/logo3.png" alt="logo" width="45px" height="45px"></div-col-xs-3>
+  <div-col-xs-9>TechnoEvent</div-col-xs-9>
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="page1.php"><i class="fa fa-home" aria-hidden="true">Home</i>
+</a></li>
+  
+                    <li><a href="contact.php"><i class="fa fa-comments" aria-hidden="true"></i>
+ Contact Us</a></li>
+               <li><a href="register/logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i>
+
+
+ log out</a></li>
+               
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav> 
+  
+    <div class="jumbotron">
+        <div class="container">
+            <div id="detail" class="animated fadeInLeft">
+                <h1>TechnoEvent</h1>
+                <p> Welcome To the TechnoEvent.</p>
+            </div>
+        </div> <img src="images/re12.png" alt="jumbotron" width="100%"> </div>
+      
+          <div class="container">
+ 
+  <div class="row">
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="sili.html">
+          <img src="images/sili.jpg" alt="Lights" style="width:100%">
+          <div class="caption">
+            <h3><center>Silicus</center></h3>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="1-2.html" target="_blank">
+          <img src="images/robo.jpg" alt="Fjords" style="width:100%">
+          <div class="caption">
+           <h3><center> Robo del Barcos</center></h3>
+          </div>
+        </a>
+      </div>
+    </div>
+    
+    <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="1-3.html" target="_blank">
+          <img src="images/rob.jpg" alt="Fjords" style="width:100%">
+          <div class="caption">
+           <h3><center> Robustus</center></h3>
+          </div>
+        </a>
+      </div>
+    </div>
+    
+    
+
+      
+      
+      
+      <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="2-1.html" target="_blank">
+          <img src="images/tech.jpg" alt="Fjords" style="width:100%">
+          <div class="caption">
+           <h3><center>Technorion</center></h3>
+          </div>
+        </a>
+      </div>
+    </div>
+      
+      
+      
+      
+      <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="2-2.html" target="_blank">
+          <img src="images/water.jpg" alt="Fjords" style="width:100%">
+          <div class="caption">
+           <h3><center> Water Rocket</center></h3>
+          </div>
+        </a>
+      </div>
+    </div>
+      
+      
+      
+      <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="dyn.html" target="_blank">
+          <img src="images/dyn.jpg" alt="Fjords" style="width:100%">
+          <div class="caption">
+           <h3><center>Dynamus</center></h3>
+          </div>
+        </a>
+      </div>
+    </div>
+       
+     
+
+<div class="col-md-4">
+      <div class="thumbnail">
+        <a href="3-1.html" target="_blank">
+          <img src="images/goog.jpg" alt="Fjords" style="width:100%">
+          <div class="caption">
+           <h3><center>Googler</center></h3>
+          </div>
+        </a>
+      </div>
+    </div> 
+    
+      
+        <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="3-2.html" target="_blank">
+          <img src="images/img3.jpg" alt="Fjords" style="width:100%">
+          <div class="caption">
+           <h3><center>Structra</center></h3>
+          </div>
+        </a>
+      </div>
+    </div>   
+    
+      <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="#" target="_blank">
+          <img src="images/estate.jpg" alt="Fjords" style="width:100%">
+          <div class="caption">
+           <h3><center>Estate War</center></h3>
+          </div>
+        </a>
+      </div>
+    </div> 
+    
+    
+        <div class="col-md-4">
+      <div class="thumbnail">
+        <a href="/w3images/fjords.jpg" target="_blank">
+          <img src="images/rm.jpg" alt="Fjords" style="width:100%">
+          <div class="caption">
+           <h3><center>RoboMaze</center></h3>
+          </div>
+        </a>
+      </div>
+    </div> 
+  </div>
+</div>
+
+           
+           
+           
+           
+<footer >
+    <div class="TechnoEvent"> <center>&copy; Copyrights By www.technoevent.com All Rights Reserved   </center></div>
+    </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+  </body>
+</html>  
+
