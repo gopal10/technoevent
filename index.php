e1408117dde6a441ba87615d3fd8dc31f6ebb8ee
gopal10 <gopalpanchal10@gmail.com>
2017-04-29 19:45:35 +0530

    sample webpages
    starting pages

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <title>Welcome to TechnoEvent</title>
+
+       <link rel="stylesheet" href="css/style.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+ <link href="css/style.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="images/logo3.png">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <link rel="stylesheet" href="css/animated.css">
+  </head>
+  <body>
+   
+     <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+                <a class="navbar-brand" href="index.php">
+                    <div-col-xs-3><img src="images/logo3.png" alt="logo" width="45px"></div-col-xs-3>
+                    <div-col-xs-9>TechnoEvent</div-col-xs-9>
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.php"><i class="fa fa-home" aria-hidden="true">Home</i>
+</a></li>
+                    <li><a href="login.php"><i class="fa fa-sign-in" aria-hidden="true"></i>
+
+ Login</a></li>
+                    <li><a href="register.php"><i class="fa fa-sign-in" aria-hidden="true"></i>
+
+ Register</a></li>
+                    
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+    <div class="jumbotron">
+        <div class="container">
+            <div id="detail" class="animated fadeInLeft">
+                <h1>TechnoEvent</h1>
+                <p> Welcome To the TechnoEvent.</p>
+            </div>
+        </div> <img src="images/re12.png" alt="jumbotron" width="100%"> </div>
+
+      
+
+
+<div class="container">
+<h2>Click Here to proceed</h2>
+
+
+    <a href="page1.php"> <button class="button" style="vertical-align:middle"><span>Go... </span></button>
+    </a>
+     </div>
+      
+<footer >
+    <div class="TechnoEvent"> <center>&copy; Copyrights By www.technoevent.com All Rights Reserved   </center></div>
+    </footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+  </body>
+</html>  
+
